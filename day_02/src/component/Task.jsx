d1c4dbe1caa22b543a8f5c18e183244d21e8662d
zubayer <zubayer.org@gmail.com>
2025-01-13 21:42:45 +0600

    first task

---
+++ day_02/src/component/Task.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Task = () => {
+  return (
+    <div className="w-[450px] h-[200px] mx-auto text-center bg-gray-100 rounded-lg shadow-md">
+      <h1 className="text-xl text-green-700 py-4">
+        We are here to learn React from tapaScript
+      </h1>
+    </div>
+  );
+};
+
+export default Task;
